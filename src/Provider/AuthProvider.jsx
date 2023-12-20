ee8f7cc64ef9a80057f33b8ef0f4fce4e4262d8b
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-12-21 01:29:35 +0600

    Firebase added

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,11 @@
+
+
+const AuthProvider = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AuthProvider;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAd8E0MNmGmLP3zHnyR-j-97YHyF96Ruws",
+  authDomain: "task-7f874.firebaseapp.com",
+  projectId: "task-7f874",
+  storageBucket: "task-7f874.appspot.com",
+  messagingSenderId: "733065280053",
+  appId: "1:733065280053:web:53181327ede1deadfccf7c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
